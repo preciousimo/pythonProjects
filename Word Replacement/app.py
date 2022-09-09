1b4353cc738b0c3f4484b3a6b4d9d2ab09d9f84c
preciousimo <preciousimoniakemu@gmail.com>
2022-09-09 07:23:19 +0100

    Word Replacement Program

---
+++ Word Replacement/app.py
@@ -0,0 +1,9 @@
+def replace_word():
+
+    str = "Hi guys, i am Precious, and hi hi"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
